0cdc5aaee860c56fba915f58a0aec217626886b4
Masaaki Murakami <masaaki.murakami@gmail.com>
2025-08-16 12:44:05 +1200

    Added errata file

---
+++ resources/errata.js
@@ -0,0 +1,12 @@
+
+const errata = {
+	"抸": "52037",
+	"契": "57430",
+};
+
+for (let i = 0; i < fourCorner.length; i++) {
+	const kanji = fourCorner[i][0];
+	if (kanji in errata) {
+		fourCorner[i][1] = errata[kanji];
+	}
+}
